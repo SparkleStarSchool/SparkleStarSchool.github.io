8856adeb2c9bc7a611ddcee52981117dd962d4d8
Aurora2021 <aurorawangca@gmail.com>
2022-08-17 18:29:00 -0600

    update home page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,6 +15,9 @@ $(document).ready( function() {
         let path = "assets/images/French.jpeg"
         appendImage(path)
     })
+    $('#lightbox').find('.book-course').on('click',()=>{
+        mailTo()
+    })
 });
 
 const appendImage = (path)=>{
@@ -23,3 +26,13 @@ const appendImage = (path)=>{
     $('#lightbox').find('.modal-body').append($img)
     $('#lightbox').modal('show')
 } 
+
+const mailTo = ()=>{
+    // who=prompt("Enter recipient's email address: ","antispammer@earthling.net");
+    who = "aurorawangca@gmail.com"
+    what= "test"
+    parent.location.href='mailto:'+who+'?subject='+what+'';
+    // if (confirm("Are you sure you want to mail "+who+" with the subject of "+what+"?")==true){
+    // parent.location.href='mailto:'+who+'?subject='+what+'';
+    //   }
+}
