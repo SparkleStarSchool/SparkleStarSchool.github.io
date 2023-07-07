756bc5faa3fed99eb6d6aeabe0544c0815077bbd
Aurora <aurorawangca@gmail.com>
2023-07-07 15:47:22 -0600

    add video and news

--- js/index.js
+++ js/index.js
@@ -18,6 +18,50 @@ $(document).ready( function() {
     $(".navbar-nav li a").on('click', ()=>{
         $('#navbarSupportedContent').removeClass('show')
     })
+
+    // slick-carousel
+    $('#main-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        infinite: true,
+        arrows: true,
+        // adaptiveHeight: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true,
+              // adaptiveHeight: true
+            }
+          },
+          {
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              infinite: true,
+              dots: true,
+              // adaptiveHeight: true
+            }
+          },
+          {
+            breakpoint: 550,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true,
+              // adaptiveHeight: true
+            }
+          }
+        ]
+      });
 });
 
 const appendImage = (path)=>{
--- js/module/header.js
+++ js/module/header.js
@@ -14,25 +14,25 @@ $(document).ready(()=>{
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto navStyle">
                         <li class="nav-item active">
-                            <a class="nav-link " href="/index.html">主页</a>
+                            <a class="nav-link " href="./index.html">主页</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/index.html#learn">关于启明</a>
+                            <a class="nav-link" href="./index.html#about-us">关于启明</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/courses.html">课程推荐</a>
+                            <a class="nav-link" href="./courses.html">课程推荐</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/club.html">俱乐部<img src="img/free3.png" alt=""></a>
+                            <a class="nav-link" href="./courses.html">俱乐部<img src="./img/free3.png" alt=""></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/schedule.html">课表汇总</a>
+                            <a class="nav-link" href="./schedule.html">课表汇总</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/index.html#teacher">课堂展示</a>
+                            <a class="nav-link" href="./index.html#show">课堂展示</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/index.html#contact">联系方式</a>
+                            <a class="nav-link" href="./index.html#contact">联系方式</a>
                         </li>
                     </ul>
                 </div>
