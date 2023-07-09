fd346423851f23ed4a530b558fdf46beee11fe28
Aurora <aurorawangca@gmail.com>
2023-07-08 20:57:53 -0600

    update dropdown menu

--- js/index.js
+++ js/index.js
@@ -1,24 +1,4 @@
 $(document).ready( function() {
-    $('#course1').on('click',()=>{
-        let path = "assets/images/StoryOfChinese.jpeg"
-        appendImage(path)
-    })
-    $('#course2').on('click',()=>{
-        let path = "assets/images/Math.jpeg"
-        appendImage(path)
-    })
-    $('#course3').on('click',()=>{
-        let path = "assets/images/SaveSoil.jpeg"
-        appendImage(path)
-    })
-    $('#course4').on('click',()=>{
-        let path = "assets/images/French.jpeg"
-        appendImage(path)
-    })
-    $(".navbar-nav li a").on('click', ()=>{
-        $('#navbarSupportedContent').removeClass('show')
-    })
-
     // slick-carousel
     $('#main-slider').slick({
         slidesToShow: 4,
@@ -63,10 +43,3 @@ $(document).ready( function() {
         ]
       });
 });
-
-const appendImage = (path)=>{
-    $img = $(`<img style='width:100%' src=${path}>`)
-    $('#lightbox').find('.modal-body').empty()
-    $('#lightbox').find('.modal-body').append($img)
-    $('#lightbox').modal('show')
-} 
